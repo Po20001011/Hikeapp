62b9c160c5127f8b265049fe7e53d66d3d76edd9
Po <qaws3588@gmail.com>
2023-07-20 19:31:43 +1000

    second commit

--- Hike/Component/CardView.swift
+++ Hike/Component/CardView.swift
@@ -9,23 +9,30 @@ import SwiftUI
 
 struct CardView: View {
     var body: some View {
+        // MARK: - CARD
+        
+        
         ZStack {
-            Circle()
-                .fill(
-                    LinearGradient(
-                        colors: [
-                            Color("ColorIndigoMedium"), Color("ColorSalmonLight")
-                        ], startPoint: .topLeading,
-                        endPoint: .bottomTrailing
-                    )
-                )
-                .frame(width: 256, height: 256)
+            CustomBackgroundView()
             
-            Image("image-1")
-                  .resizable()
-              .scaledToFit()
-        }
-
+            ZStack {
+                Circle()
+                    .fill(
+                        LinearGradient(
+                            colors: [
+                                Color("ColorIndigoMedium"), Color("ColorSalmonLight")
+                            ], startPoint: .topLeading,
+                            endPoint: .bottomTrailing
+                        )
+                    )
+                    .frame(width: 256, height: 256)
+                
+                Image("image-1")
+                      .resizable()
+                  .scaledToFit()
+            }
+        } //: CARD
+        .frame(width: 320, height: 570)
     }
 }
 
--- Hike/Component/CustomBackgroundView.swift
+++ Hike/Component/CustomBackgroundView.swift
@@ -12,14 +12,22 @@ struct CustomBackgroundView: View {
         ZStack {
             // MARK: - 3. DEPTH
             
+            Color.customGreenDark
+                .cornerRadius(40)
+                .offset(y: 12)
             
             // MARK: - 2. LIGHT
+            
+            Color.customGreenLight
+                .cornerRadius(40)
+                .offset(y: 3)
+                .opacity(0.85)
             
             // MARK: - 1. SURFACE
             
             LinearGradient(colors: [
-                Color("ColorGreenLight"),
-                Color("ColorGreenMedium")],
+                .customGreenLight,
+                .customGreenMedium],
                 startPoint: .top,
                 endPoint: .bottom
             )
