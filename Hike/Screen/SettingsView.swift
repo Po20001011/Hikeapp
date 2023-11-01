74e4e3184b604eca454b54df6ed985fb08cf1c14
Po <qaws3588@gmail.com>
2023-11-01 14:01:33 +1100

    Update on Setting View

---
+++ Hike/Screen/SettingsView.swift
@@ -0,0 +1,20 @@
+//
+//  SettingsView.swift
+//  Hike
+//
+//  Created by Wang Po on 13/9/2023.
+//
+
+import SwiftUI
+
+struct SettingsView: View {
+    var body: some View {
+        Text("Settings View")
+    }
+}
+
+struct SettingsView_Previews: PreviewProvider {
+    static var previews: some View {
+        SettingsView()
+    }
+}
